c32e2b8f3db3d88628c79d804313fb3db48b102f
sorazo <picanarachu@outlook.com>
2024-08-04 21:50:58 +0900

    lesson3-1

---
+++ miyu_fukuhara/src/components/lesson3/index.ts
@@ -0,0 +1,7 @@
+// コンポーネントの部品を呼び出すためのexportをまとめている
+export { PageTitle } from "./page-title";
+export { Text } from "./text";
+export { SectionTitle } from "./section-title";
+export { DefinitionList } from "./definition-list";
+export { List, ListItem } from "./list";
+export { Contents } from "./contents";
